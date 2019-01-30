337ee5abb02c858f361b0ca8e07bad04f395fd19
Paras Sanghavi <insomniac18@gmail.com>
2019-01-30 01:26:51 -0800

    new page for search

---
+++ pages/artist_search.js
@@ -0,0 +1,66 @@
+import querystring from 'querystring'
+import axios from "axios";
+
+function extractArtistNames(response) {
+  return response.data.artists.items.map((artist) => (artist.name))
+}
+
+class ArtistSearch extends React.Component {
+  componentDidMount() {
+    this.setState({accessToken: querystring.parse(window.location.hash)['#access_token']});
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {value: '', artists: [], accessToken: null};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.get('https://api.spotify.com/v1/search',
+      {
+        params: {
+          type: 'artist',
+          q: this.state.value
+        },
+        headers: {
+          'Authorization': 'Bearer ' + this.state.accessToken
+        }
+      })
+      .then((response) => (
+        this.setState({artists: extractArtistNames(response)})
+      ))
+      .catch((error) => (
+        console.log(error)
+      ))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>Enter your favorite artist:</p>
+        <p>Artists:</p>
+        <ul>
+          {this.state.artists.map((artist) =>
+            <li>{artist}</li>
+          )}
+        </ul>
+        <label>
+          Artist:
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
+}
+
+export default ArtistSearch
--- pages/index.js
+++ pages/index.js
@@ -1,94 +1,16 @@
 import querystring from 'querystring'
-import axios from 'axios'
 
 const spotify_params = {
   client_id: process.env.SPOTIFY_CLIENT_ID,
-  redirect_uri: process.env.BASE_URI,
+  redirect_uri: `${process.env.BASE_URI}/artist_search`,
   response_type: 'token'
 };
-const query_params = querystring.stringify(spotify_params);
-const SPOTIFY_AUTHORIZE_URL = `https://accounts.spotify.com/authorize?${query_params}`;
-
-function extractArtistNames(response) {
-  return response.data.artists.items.map((artist) => (artist.name))
-}
-
-class ArtistForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {value: '', artists: []};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    axios.get('https://api.spotify.com/v1/search',
-      {
-        params: {
-          type: 'artist',
-          q: this.state.value
-        },
-        headers: {
-          'Authorization': 'Bearer ' + this.props.accessToken
-        }
-      })
-      .then((response) => (
-        this.setState({artists: extractArtistNames(response)})
-      ))
-      .catch((error) => (
-        console.log(error)
-      ))
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>Artists:</p>
-        <ul>
-          {this.state.artists.map((artist) =>
-            <li>{artist}</li>
-          )}
-        </ul>
-        <label>
-          Artist:
-          <input type="text" value={this.state.value} onChange={this.handleChange}/>
-        </label>
-        <input type="submit" value="Submit"/>
-      </form>
-    );
-  }
-}
-
-class IndexPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {accessToken: null}
-  }
-
-  componentDidMount() {
-    this.setState({accessToken: querystring.parse(window.location.hash)['#access_token']});
-  }
-
-  render() {
-    return (
-      <div>
-        <a href={SPOTIFY_AUTHORIZE_URL}>First, please log in to Spotify.</a>
-
-        <p>Enter your favorite artist:</p>
-        <ArtistForm accessToken={this.state.accessToken}/>
-      </div>
-    )
-  }
-}
+const SPOTIFY_AUTHORIZE_URL = `https://accounts.spotify.com/authorize?${querystring.stringify(spotify_params)}`;
 
 export default () => (
-  <IndexPage/>
+  <div>
+    <h1>Spoti5</h1>
+    <p>Welcome!</p>
+    <a href={SPOTIFY_AUTHORIZE_URL}>First, please log in to Spotify.</a>
+  </div>
 )
